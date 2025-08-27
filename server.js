3a620c365a64632ffd8ed9b82b746b0896716a09
polar <polar@dona.one>
2025-08-27 12:56:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const request = require('request');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,6 +13,17 @@ app.get('/proxy', (req, res) => {
         res.send('<p>No URL provided.</p>');
     }
 });
+
+app.use('/proxy', createProxyMiddleware({
+    target: 'https://soundcloud.com', // Change this to the target URL you want to proxy
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('Host', 'soundcloud.com'); // Set the Host header to the target domain
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
